5396964d78aaa6e8000285ea20315a1dac1bb047
Aric Aggarwal <aric.aggarwal@GCCYJTVCJ1WK.local>
2020-09-10 09:26:44 -0400

    first push

---
+++ Demo_Projects/sorting_list.py
@@ -0,0 +1,20 @@
+list = [3,6,5,3,4,6,7]
+print(list)
+list.sort()
+print(list)
+
+#Question:
+#How many functions am I using in this program?
+#Answer=2
+#Question: What is the difference between print adn sort?
+#Print is a static method meaning it called by itself 
+#sort is an instance method meaning it is called with an instance 
+
+str = "Paul"
+str = str.upper()
+print (str)
+
+#Take Awy:
+#When talking about functions always ask yourself 
+# 1. What is the name of the Function 
+# 2. What doe sthe function take - parameters 
