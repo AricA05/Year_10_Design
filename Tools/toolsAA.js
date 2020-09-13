61a2499d34bf3445e6364702f82d8b428873ea1f
Aric Aggarwal <aric.aggarwal@GCCYJTVCJ1WK.local>
2020-09-13 17:54:50 -0400

    push

---
+++ Tools/toolsAA.js
@@ -0,0 +1,11 @@
+#isEven tool
+
+function isEven(a) {
+	
+	if (a % 2 == 0){
+		return true;
+	}
+
+#Otherwise:
+		return false;
+}
--- Tools/toolsAA.py
+++ Tools/toolsAA.py
@@ -1,3 +1,5 @@
+#Tool 1: isEven tool, this tool determines if a number is odd or even.
+#If number is even, it returns true, odd is false
 '''isEven tool takes a single integer parameter a>=0 returning true if
 a is even and false if a is odd
 '''
@@ -13,3 +15,28 @@ def isEven(a):
 print(isEven(0))
 print(isEven(10))
 print(isEven(9))
+
+
+
+#Tool 2: missing char tool 
+def missing_char(str, n):
+  
+  newStr = ""
+  newStr = str[0:n] + str[n + 1: len(str)]
+  return newStr
+
+'''
+How this was solved layout:
+012345    missing_char ("kitten",1) --> ktten    str[0:1] + str[2:6]
+kitten    missing_char ("kitten",4) --> kittn    str[0:4] + str[5:6]
+
+Now, Generalize the two examples to get a solution:
+
+str[0:n] + str[n + 1: len(str)]
+
+Notes:
+- len(str) means length of string. The string is the word, so in this case
+it is kitten
+- missing_char means missing character (letter)
+- n is the missing character 
+'''
