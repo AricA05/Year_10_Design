bcf85f9ac9d6e88f80675a0e5f501e27c3951b0b
Aric Aggarwal <aric.aggarwal@GCCYJTVCJ1WK.local>
2020-09-22 13:52:36 -0400

    push

--- Ferengi_Currency_Relocation_Project/accessAPI.py
+++ Ferengi_Currency_Relocation_Project/accessAPI.py
@@ -1,7 +1,7 @@
 import requests
 import json
 from pprint import pprint
-
+import tkinter as tk
 
 #Get Key
 #This is a file not in my respository I don't want you to have it
@@ -17,4 +17,44 @@ resp = requests.get('http://data.fixer.io/api/latest?access_key='+key)
 #Converts response to JSON
 data = resp.json()
 
-pprint(data)
+#pprint(data)
+#print(data["timestamp"])
+#print(data["base"])
+#print(data["rates"]["CAD"])
+
+country = []
+value = []
+
+#loops through the dictionary at data["rates"]
+for key in data["rates"]:
+	#print(key, ":",data["rates"][key])
+
+	country.append(key)#append the key into country list
+	value.append(data["rates"][key])#append the value into value list 
+
+
+print(country)
+print(value)
+
+def change(x):
+	print(x)
+	print(data["rates"][x])
+
+root = tk.Tk()#creates main window for GUI program 
+#Build/contruct GUI here before root.mainloop
+#Step 1: Construct the widget/object
+#Step 2: Configure widget/object
+#Step 3: Place it on the frame/main window
+
+lab = tk.Label(root, text = "Select Currency")#contruction
+
+var = tk.StringVar(root)
+var.set(country[0])
+option_menu = tk.OptionMenu(root, var, *country, command = change)
+
+lab.pack() #placing 
+option_menu.pack()
+
+
+root.mainloop()#executes an infinite loop waiting for the user to do something 
+print("END")
