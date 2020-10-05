432c22af515c58b61b0035731224ee5949b2bc0d
Aric Aggarwal <aric.aggarwal@GCCYJTVCJ1WK.local>
2020-10-04 21:35:42 -0400

    push

--- Project_1_Work/Planning_Materials/buttons_practice_planning.py
+++ Project_1_Work/Planning_Materials/buttons_practice_planning.py
@@ -1,21 +1,22 @@
 from tkinter import * #GUI package
-window = Tk()
-window.title("button practice code") 
-window.geometry('500x300+0+0')
+window = Tk() #window
+window.title("button practice code") #winodw title 
+window.geometry('500x300+0+0') #dimensions for window size
 header = Label(window, text="button practice", font=("arial",30,"bold"), fg="steelblue").pack()
+#Label type = header with font and colour modifications
 
-window.configure(bg='skyblue')
+window.configure(bg='skyblue') #configures background colour (bg)
 
 L1 = Label(window, text = "click button", font=("arial", 18)).place(x=10,y=100)
+#more practice with label creation and fonts/text 
 
-#window = Tk()
 
 def new_winF(): # new window definition
-    newwin = Toplevel(window)
-    display = Label(newwin, text="window opened from button click")
-    display.pack()    
+    newwin = Toplevel(window)#opens new window 
+    display = Label(newwin, text="window opened from button click")#displayed in new window
+    display.pack() 
 
 button1 =Button(window, text ="this is a button", command =new_winF)#command linked
-button1.place(x=200,y=140)
+button1.place(x=200,y=140)#placement of button
 
-window.mainloop()
+window.mainloop()#tkinter event loop
--- Project_1_Work/Planning_Materials/label_and_header_practice.py
+++ Project_1_Work/Planning_Materials/label_and_header_practice.py
@@ -1,17 +1,21 @@
 from tkinter import * #GUI package
 
-window = Tk()
-window.title("Practice Labeling and Title's") 
-window.geometry('700x600+0+0')
-header = Label(window, text="This is a header/title example!", font=("arial",30,"bold"), fg="steelblue").pack()
+window = Tk() #window
+window.title("Practice Labeling and Title's") #creates a title for window
+window.geometry('700x600+0+0') #sizeing of window
+header = Label(window, text="This is a header/title example!", font=("arial",30,"bold"), fg="steelblue").pack() 
+#Label type = header with font and colour modifications
 
-window.configure(bg='skyblue')
+window.configure(bg='skyblue') #configures background colour (bg) of window
 
-L1 = Label(window, text = "Exercise", font=("arial", 18)).place(x=10,y=100)
+L1 = Label(window, text = "Exercise", font=("arial", 18)).place(x=10,y=100) 
+#all of these are different label's pretty much just simple writing 
 L2 = Label(window, text = "Day (dd)", font=("arial",18)).place(x=10,y=150)
 L3 = Label(window, text = "Month (mm)", font=("arial",18)).place(x=10,y=200)
 L4 = Label(window, text = "Year (yyyy)", font=("arial",18)).place(x=10,y=250)
 L5 = Label(window, text = "Reps per set ", font=("arial",18)).place(x=10,y=300)
 L6 = Label(window, text = "Sets completed", font=("arial",18)).place(x=10,y=350)
 
-window.mainloop()
+window.mainloop() 
+'''runs the Tkinter event loop, waits for any action to occur on page, 
+proccess' action/event as long as window isn't closed'''
