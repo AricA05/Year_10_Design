308504b7c445e3dc780c1e7d9d85ee4f0498443d
Aric Aggarwal <aric.aggarwal@GCCYJTVCJ1WK.local>
2020-11-09 02:08:41 -0500

    push

---
+++ Project_1_Work/Final Documents for Project 1/api.py
@@ -0,0 +1,12 @@
+import requests
+results = requests.get('https://wger.de/api/v2/equipment/')
+results_json = results.json()
+#To print the required information
+for p in results_json['results']:
+    print(p['id'])
+for i in results_json['results']:
+	print(i['name'])
+
+#https://medium.com/quick-code/absolute-beginners-guide-to-slaying-apis-using-python-7b380dc82236#:~:text=To%20interact%20with%20an%20API,in%20an%20organized%2C%20logical%20manner.
+
+#https://wger.de/api/v2/equipment/
