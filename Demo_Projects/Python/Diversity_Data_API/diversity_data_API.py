e856aadf1656d0e21883658553846c105b3c1004
Aric Aggarwal <aric.aggarwal@GCCYJTVCJ1WK.local>
2020-11-30 22:16:29 -0500

    push

--- Demo_Projects/Python/Diversity_Data_API/diversity_data_API.py
+++ Demo_Projects/Python/Diversity_Data_API/diversity_data_API.py
@@ -5,8 +5,8 @@ from pprint import pprint
 fname = input("What is the first name: ")
 lname = input("What is the last name:")
 
-#resp = requests.get("https://api.diversitydata.io/?fullname="+fname+"%20"+lname)
-resp = requests.get("https://jsonplaceholder.typicode.com/posts")
+resp = requests.get("https://api.diversitydata.io/?fullname="+fname+"%20"+lname)
+#resp = requests.get("https://jsonplaceholder.typicode.com/posts")
 
 data = resp.json()
 
