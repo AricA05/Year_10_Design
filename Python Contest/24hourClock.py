945746caf6b7d70d741fd00414b66baa7caf9b47
Aric Aggarwal <aric.aggarwal@GCCYJTVCJ1WK.local>
2021-01-19 09:30:38 -0500

    push

---
+++ Python Contest/24hourClock.py
@@ -0,0 +1,3 @@
+#Problem: https://www.nayuki.io/res/dwite-programming-contest-solutions/dwite-200410-p2.pdf
+
+
---
+++ Python Contest/areaOfCircle.py
@@ -0,0 +1,16 @@
+import math
+
+#Problem: https://www.nayuki.io/res/dwite-programming-contest-solutions/dwite-200410-p1.pdf
+
+
+x1 = 2
+y1 = 4
+x2 = 4
+y2 = 8
+
+
+r = math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1))
+
+a = 3.14159*r*r
+
+print(a)
