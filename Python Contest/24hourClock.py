7aefe71c709714e04aa3c3ab7e4352d2ea89f1e9
Aric Aggarwal <aric.aggarwal@GCCYJTVCJ1WK.local>
2021-01-19 21:29:37 -0500

    push

--- Python Contest/24hourClock.py
+++ Python Contest/24hourClock.py
@@ -1,3 +1,50 @@
 #Problem: https://www.nayuki.io/res/dwite-programming-contest-solutions/dwite-200410-p2.pdf
 
 
+#this only works with decimals, not colon signs
+'''def conversion():
+	t = 17.12
+
+	if 0 <= t < 12:
+		print(t, "AM")
+
+	if 12 <= t < 24:
+		print(t-12, "PM") 
+
+conversion()'''
+
+
+
+#Solution 1
+'''time = "18:12"
+XM = str
+
+hour = int(time[:2])
+minute = (time[-2:])
+
+hour2 = (hour - 12)
+
+if 0 <= hour < 12:
+	print(hour,":",minute, "AM")
+
+if 12 <= hour < 24:
+	print(hour2,":",minute, "PM")'''
+
+
+
+#Solution 2
+
+val = "09:12"
+
+h = int(val[:2])
+
+m = (val[-2:])
+
+timeOfDay = "AM"
+
+
+if 12 <= h <= 23:
+	timeOfDay = "PM";
+	h = h - 12
+	
+print(h,":",m," ",timeOfDay)
