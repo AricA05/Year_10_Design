376797563f34b169c9661d716153e85066bc4f06
Aric Aggarwal <aric.aggarwal@GCCYJTVCJ1WK.local>
2021-04-01 15:03:54 -0400

    push

---
+++ Binary_Functions/addbinary.py
@@ -0,0 +1,15 @@
+'''binary1 = "0110"
+binary2 = "0001"
+
+integer_sum = int(binary1, 2) + int(binary2, 2)
+binary_sum = bin(integer_sum)
+print(binary_sum)'''
+
+
+a = '00001'
+b = '01111'
+
+c = bin(int(a,2) + int(b,2))
+print(c)
+# 0b100
+
