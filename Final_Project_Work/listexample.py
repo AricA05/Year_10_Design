dea78d1dcd077d7d9d3c7d1383905fa7e00021a2
Aric Aggarwal <aric.aggarwal@GCCYJTVCJ1WK.local>
2021-05-05 11:11:10 -0400

    push

---
+++ Final_Project_Work/listexample.py
@@ -0,0 +1,9 @@
+import random as random
+
+
+questions = ["What is the velocity?", "What is the speed?", "What is the displacement?", "Total time?"]
+
+
+q2 = random.choice(questions)
+
+print(q2)
---
+++ Final_Project_Work/randomization_test.py
@@ -0,0 +1,5 @@
+from random import randint
+
+R = randint(1,100)
+
+print(R)
